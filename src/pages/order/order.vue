41df2c1cff4292963b0b79dee81f181ec22f53a9
GanYihuan <583520052@qq.com>
2019-07-06 01:06:43 +0800

    order better-scroll

--- src/pages/order/order.vue
+++ src/pages/order/order.vue
@@ -1,63 +1,68 @@
 ﻿<template>
-  <div class="order">
+  <div class="orderPage">
     <div class="header">
       订单
     </div>
-    <div class="order-list">
-      <div
-        v-for="(item, index) in orders"
-        :key="index"
-        class="order-item"
-      >
-        <div class="order-item-inner">
-          <img
-            class="item-img"
-            :src="item.sellerImage"
-          >
-          <div class="item-right">
-            <div
-              class="item-top"
+    <div
+      ref="order"
+      class="order"
+    >
+      <div class="order-list">
+        <div
+          v-for="(item, index) in orders"
+          :key="index"
+          class="order-item"
+        >
+          <div class="order-item-inner">
+            <img
+              class="item-img"
+              :src="item.sellerImage"
             >
-              <p class="order-name one-line">
-                {{ item.sellerName }}
-              </p>
+            <div class="item-right">
               <div
-                class="close"
-                @click="deleteComment(item.sellerName)"
+                class="item-top"
               >
-                <i class="icon-close" />
-              </div>
-            </div>
-            <div class="item-bottom">
-              <div
-                v-for="(mitem, mindex) in item.menu"
-                :key="mindex"
-              >
-                <div class="product-item">
-                  {{ mitem.name }}
-                  <div class="p-count">
-                    x {{ mitem.count }}
-                  </div>
+                <p class="order-name one-line">
+                  {{ item.sellerName }}
+                </p>
+                <div
+                  class="close"
+                  @click="deleteComment(item.sellerName)"
+                >
+                  <i class="icon-close" />
                 </div>
               </div>
-              <div class="p-total-count">
-                总计{{ item.number }}个菜，实付<span class="total-price">¥{{ item.price }}</span>元
+              <div class="item-bottom">
+                <div
+                  v-for="(mitem, mindex) in item.menu"
+                  :key="mindex"
+                >
+                  <div class="product-item">
+                    {{ mitem.name }}
+                    <div class="p-count">
+                      x {{ mitem.count }}
+                    </div>
+                  </div>
+                </div>
+                <div class="p-total-count">
+                  总计{{ item.number }}个菜，实付<span class="total-price">¥{{ item.price }}</span>元
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div class="evaluation clearfix">
-          <router-link
-            to="/evaluate"
-            tab="div"
-          >
-            <div
-              class="evaluation-btn"
-              @click="showEvalutate(index)"
+          <div class="evaluation clearfix">
+            <router-link
+              to="/evaluate"
+              tab="div"
             >
-              评价
-            </div>
-          </router-link>
+              <div
+                class="evaluation-btn"
+                @click="showEvalutate(index)"
+              >
+                评价
+              </div>
+            </router-link>
+          </div>
         </div>
       </div>
     </div>
@@ -68,6 +73,7 @@
 import axios from 'axios'
 import { mapMutations } from 'vuex'
 import { Notyf } from 'notyf' // 纯js消息通知插件
+import BScroll from 'better-scroll'
 
 export default {
   name: 'Order',
@@ -81,9 +87,19 @@ export default {
     if (status === 200) {
       this.orders = orders
     }
+    this._initScroll()
   },
   methods: {
     ...mapMutations({ setEvaluateIndex: 'SET_EVALUATEINDEX' }),
+    _initScroll() {
+      if (!this.scroll) {
+        this.scroll = new BScroll(this.$refs.order, {
+          click: true
+        })
+      } else {
+        this.scroll.refresh()
+      }
+    },
     showEvalutate(index) {
       this.setEvaluateIndex(index)
       this.$router.push(`/evaluate`)
