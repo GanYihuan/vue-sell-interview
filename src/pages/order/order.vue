9af13251c02078710f41d24d2eefce550025396c
GanYihuan <583520052@qq.com>
2019-07-05 15:30:00 +0800

    order f

--- src/pages/order/order.vue
+++ src/pages/order/order.vue
@@ -3,12 +3,73 @@
     <div class="header">
       订单
     </div>
+    <div class="order-list">
+      <div
+        v-for="(item, index) in orders"
+        :key="index"
+        class="order-item"
+      >
+        <div class="order-item-inner">
+          <img
+            class="item-img"
+            src="item"
+          >
+          <div class="item-right">
+            <div
+              class="item-top"
+            >
+              <p class="order-name one-line">
+                {{ item.seller }}
+              </p>
+              <i class="icon-keyboard_arrow_right" />
+            </div>
+            <div class="item-bottom">
+              <div
+                v-for="(mitem, mindex) in item.menu"
+                :key="mindex"
+              >
+                <div class="product-item">
+                  {{ mitem.name }}
+                  <div class="p-count">
+                    x {{ mitem.count }}
+                  </div>
+                </div>
+              </div>
+              <div class="p-total-count">
+                总计{{ item.number }}个菜，实付<span class="total-price">¥{{ item.price }}</span>元
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="evaluation clearfix">
+          <div
+            class="evaluation-btn"
+          >
+            评价
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'Order'
+  name: 'Order',
+  data() {
+    return {
+      orders: []
+    }
+  },
+  async mounted() {
+    const { status, data: { orders }} = await axios.get('/orders/getOrder')
+    if (status === 200) {
+      this.orders = orders
+    }
+    console.log(this.orders, 'orders--')
+  }
 }
 </script>
 
