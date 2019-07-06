fe92d07273da36c52d580fcf48dfc62eedf954ef
GanYihuan <583520052@qq.com>
2019-07-06 20:03:52 +0800

    header html

--- src/pages/home/header.vue
+++ src/pages/home/header.vue
@@ -1,10 +1,23 @@
 ﻿<template>
   <div class="header">
+    <div class="search-bar">
+      <div class="bar-location">
+        <div class="location-icon" />
+        <div class="location-text">
+          广州市
+        </div>
+      </div>
+      <div class="search-btn">
+        <p class="place-holder">
+          搜索
+        </p>
+      </div>
+    </div>
     <div class="banner-img" />
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
 export default {
   name: 'Header'
 }
