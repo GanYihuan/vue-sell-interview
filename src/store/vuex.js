eb5e7eec2192615991a882976fe44153f77868c2
GanYihuan <583520052@qq.com>
2019-07-08 17:48:25 +0800

    change vuex, add module.hot.accept

--- src/store/vuex.js
+++ src/store/vuex.js
@@ -8,9 +8,43 @@ Vue.use(Vuex)
 
 const debug = process.env.NODE_ENV !== 'production' // development mode is on, strict mode check vuex data
 
-export default new Vuex.Store({
-  state,
-  mutations,
-  strict: debug,
-  plugins: debug ? [createLogger()] : []
-})
+// export default new Vuex.Store({
+//   state,
+//   mutations,
+//   strict: debug,
+//   plugins: debug ? [createLogger()] : [],
+//   modules: {}
+// })
+
+export default () => {
+  const store = new Vuex.Store({
+    state,
+    mutations,
+    strict: debug,
+    plugins: debug ? [createLogger()] : [],
+    modules: {}
+  })
+  if (module.hot) { // vuex Hot replacement function
+    module.hot.accept(
+      [
+        './state/state',
+        './mutations/mutations',
+        './actions/actions',
+        './getters/getters'
+      ],
+      () => {
+        const newState = require('./state/state').default
+        const newMutations = require('./mutations/mutations').default
+        const newActions = require('./actions/actions').default
+        const newGetters = require('./getters/getters').default
+        store.hotUpdate({
+          state: newState,
+          mutations: newMutations,
+          getters: newGetters,
+          actions: newActions
+        })
+      }
+    )
+  }
+  return store
+}
