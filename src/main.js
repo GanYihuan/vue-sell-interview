25f4e9a3d09134b76d65cf23dc936f3228f3fe55
GanYihuan <583520052@qq.com>
2019-07-09 22:43:39 +0800

    main.js

--- src/main.js
+++ src/main.js
@@ -1,29 +1,23 @@
 import Vue from 'vue'
-
 import App from './App.vue'
 import router from './router'
 import store from 'store/vuex'
-
 import VueAwesomeSwiper from 'vue-awesome-swiper' // Carousel map
-import 'swiper/dist/css/swiper.css'
-Vue.use(VueAwesomeSwiper)
-
 import fastclick from 'fastclick' // eliminate mobile-end click 300MS delay
-fastclick.attach(document.body)
-
 import VueLazyload from 'vue-lazyload' // Vue image lazy loading plug-in
-Vue.use(VueLazyload, {
-  loading: require('../public/favicon.png')
-})
-
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-Vue.use(ElementUI)
-
+import 'swiper/dist/css/swiper.css'
 import 'notyf/notyf.min.css' // Pure js message notification plugin
 import 'assets/reset.css'
 import 'assets/stylus/index.styl'
 
+fastclick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
+Vue.use(VueLazyload, {
+  loading: require('../public/favicon.png')
+})
+Vue.use(ElementUI)
 Vue.config.productionTip = false // close Vue production tip
 
 router.beforeEach((to, from, next) => {
