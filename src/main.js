16299ed2e51e742c1d40fd28069ca215171700bb
GanYihuan <583520052@qq.com>
2019-07-09 22:42:21 +0800

    main.js

--- src/main.js
+++ src/main.js
@@ -1,30 +1,36 @@
 import Vue from 'vue'
+
 import App from './App.vue'
 import router from './router'
 import store from 'store/vuex'
-import VueAwesomeSwiper from 'vue-awesome-swiper' // 轮播图实现，首页的轮播图实现滚动的图标效果
-import fastclick from 'fastclick' // fastclick: 移动端点击延迟300MS
-import VueLazyload from 'vue-lazyload' // 图片延迟加载插件，实现标志图片的加载
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import 'notyf/notyf.min.css'
-import 'swiper/dist/css/swiper.css'
-import 'assets/stylus/index.styl'
-import 'assets/reset.css'
 
-fastclick.attach(document.body)
+import VueAwesomeSwiper from 'vue-awesome-swiper' // Carousel map
+import 'swiper/dist/css/swiper.css'
 Vue.use(VueAwesomeSwiper)
-Vue.use(ElementUI)
-Vue.config.productionTip = false // close Vue production tip
+
+import fastclick from 'fastclick' // eliminate mobile-end click 300MS delay
+fastclick.attach(document.body)
+
+import VueLazyload from 'vue-lazyload' // Vue image lazy loading plug-in
 Vue.use(VueLazyload, {
   loading: require('../public/favicon.png')
 })
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
+import 'notyf/notyf.min.css' // Pure js message notification plugin
+import 'assets/reset.css'
+import 'assets/stylus/index.styl'
+
+Vue.config.productionTip = false // close Vue production tip
+
 router.beforeEach((to, from, next) => {
-  if (to.matched.length === 0 || to.name === 'root') { // 如果未匹配到路由
-    from.name ? next({ name: from.name }) : next({ path: '/login' }) // 如果上级也未匹配到路由则跳转登录页面，如果上级能匹配到则转上级路由
+  if (to.matched.length === 0 || to.name === 'root') { // to.matched.length === 0 -> Unmatched route
+    from.name ? next({ name: from.name }) : next({ path: '/login' }) // from.name -> Superior routing
   } else {
-    next() // 如果匹配到正确跳转
+    next() // Jump
   }
 })
 
