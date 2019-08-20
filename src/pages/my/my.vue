1fd3425c3657c0e578fe8022ef2464f43d3b9211
GanYihuan <583520052@qq.com>
2019-08-21 02:25:14 +0800

    My

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -4,7 +4,7 @@
  * @Author: GanEhank
  * @Date: 2019-06-07 00:14:33
  * @LastEditors: GanEhank
- * @LastEditTime: 2019-08-18 23:53:54
+ * @LastEditTime: 2019-08-21 02:24:14
  -->
 <template>
   <div class="my">
@@ -23,6 +23,8 @@
             <el-col :span="5">
               用户名
             </el-col>
+            <!-- :span 占据位置 -->
+            <!-- :offset -> 间隔 -->
             <el-col
               :span="5"
               :offset="1"
